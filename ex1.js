14aa97a34eab2eb2edd9d9c76a60d2ab2be00b9a
GabrielAlencar12 <101112289+GabrielAlencar12@users.noreply.github.com>
2025-02-20 21:00:57 -0300

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,1 +1,15 @@
-alert
+function verificaridade(idade) {
+    if (idade < 12) {
+        console.log("criança");
+    } else if (idade < 18) {
+        console.log("adolecente");
+    } else if (idade < 60) {
+        console.log("adulto");
+    } else if (idade > 60) {
+        console.log("idoso");
+    } else if (idade < 0) {
+        console.log("idade invalida");
+    }
+
+}
+verificaridade(18) 
